2722b6b321424a4a4a4b475464f18f84dbf83afc
jonathanDargakis <jmdargakis@loyola.edu>
2026-03-13 23:52:41 -0400

    correct path to WebPage folder

--- Server.js
+++ Server.js
@@ -7,17 +7,10 @@ const morgan = require('morgan'); //imports morgan
 const app = express(); //creates a new Express Application
 
 app.use(morgan('dev')); //For better logging, we use morgan
-app.use( express.static('WebPage') ); // Static page server will use the folder 'WebPage'
+app.use( express.static('view/WebPage') ); // Static page server will use the folder 'WebPage'
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-
-
-
-
-
-
-
 
 
 
