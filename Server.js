c9c047dbc6872e6c0d7440bd6f728607f32aa16e
jonathanDargakis <jmdargakis@loyola.edu>
2026-03-15 23:02:44 -0400

    Test Data

--- Server.js
+++ Server.js
@@ -13,7 +13,11 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 
+app.get("/TestData",function(req,res){
+    
+});
+
 
 const server = app.listen(port,hostname,function(){ // Asynchronous Listen to client requests in hostname:port
-    console.log(`Server running on ${hostname}:${port}`); // Must be here due to the asynchronous nature of the app.listen()
+    console.log(`Server running on http://${hostname}:${port}/src/index.html`); // Must be here due to the asynchronous nature of the app.listen()
 });
