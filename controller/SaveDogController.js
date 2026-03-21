b4d8c95fca97728e32a702ea7faa66410b89dc83
jonathanDargakis <jmdargakis@loyola.edu>
2026-03-20 21:02:31 -0400

    SaveDogController basic

--- controller/SaveDogController.js
+++ controller/SaveDogController.js
@@ -1,1 +1,17 @@
 const dao = require('../model/SaveDogDao');
+
+exports.getAll = function(req,res){
+
+}
+
+exports.get = function(req,res){
+
+}
+
+exports.postCreateUpdate = function(req,res){
+
+}
+
+exports.getDelete = function(req,res){
+    
+}
