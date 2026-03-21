cb4ddea8208cff8bc40f7aff4687757b69fe5577
jonathanDargakis <jmdargakis@loyola.edu>
2026-03-20 20:58:54 -0400

    SaveDogDao model

--- model/SaveDogDao.js
+++ model/SaveDogDao.js
@@ -12,24 +12,61 @@ let lstDogs = [
 
 // Create and save a new dog 
 exports.createDog = async (dog) => {
+  let genId = lstDogs[ lstDogs.length -1 ]._id +1;
+  dog._id = genId;
 
+  lstDogs.push( dog ); //adds dog to the array 
+
+  return dog;
 };
 
 // Get all Dog
 exports.getAllDogs = async () => {
+  return lstDogs;
 };
 
 // Get an Dog by its ID
 exports.getDogById = async (id) => {
-
+  let dog = null;
+  for(let i=0; i<lstDogs.length; i++){
+    if( lstDogs[i]._id === id ){
+      dog = lstDogs[i]
+      break
+    }
+  }
+  return dog;
 };
 
-// Update Sogs info
+// Update Dogs info
 exports.update = function(dog){
+  let pos = -1;
+  for(let i=0; i<lstDogs.length; i++){
+    if( lstDogs[i]._id === dog._id ){
+      pos = i; // saves the array position of the item to update
+    }
+  }
+
+  if(pos >= 0 && pos < lstDogs.length){
+    lstDogs[pos] = dog;
+  }
 
 }
 
 // Delete an Dog by its ID
 exports.deleteDog = async (id) => {
+  let dog = null;
+  let pos = -1;
+  for(let i=0; i<lstDogs.length; i++){
+      if( lstDogs[i]._id === id ){
+          pos = i; // saves the array position of the item to delete
+      }
+  }
+
+  if(pos >= 0 && pos < lstDogs.length){
+      dog = lstDogs[pos];
+      lstDogs.splice(pos,1);
+  }
+  
+  return dog;
 
 };
