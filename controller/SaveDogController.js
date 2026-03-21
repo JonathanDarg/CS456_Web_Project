625ff40bbd51d839c21f02f0b1740e0b46e9da81
jonathanDargakis <jmdargakis@loyola.edu>
2026-03-20 22:15:06 -0400

    SaveDogController -Meghan

--- controller/SaveDogController.js
+++ controller/SaveDogController.js
@@ -1,17 +1,48 @@
 const dao = require('../model/SaveDogDao');
 
-exports.getAll = function(req,res){
+exports.getAll = function(req, res) {
+    dao.getAll().then(function(dogs) {
+        if (dogs) {
+            res.json(dogs);
+        } else {
+            res.json({ error: "No dogs found" });
+        }
+    });
+};
 
-}
+exports.get = function(req, res) {
+    const id = req.params.id;
 
-exports.get = function(req,res){
+    dao.getOne(id).then(function(dog) {
+        if (dog) {
+            res.json(dog);
+        } else {
+            res.json({ error: "Dog not found" });
+        }
+    });
+};
 
-}
+exports.postCreateUpdate = function(req, res) {
+    const dogData = req.body;
 
-exports.postCreateUpdate = function(req,res){
+    if (dogData) {
+        dao.save(dogData).then(function(result) {
+            res.json({ success: true, data: result });
+        });
+    } else {
+        res.json({ error: "No data provided" });
+    }
+};
 
-}
+exports.getDelete = function(req, res) {
+    const id = req.params.id;
 
-exports.getDelete = function(req,res){
-    
-}
+    if (id) {
+        // This is the delete function your partner added
+        dao.deleteDog(id).then(function() {
+            res.json({ deleted: true, id: id });
+        });
+    } else {
+        res.json({ error: "Missing ID" });
+    }
+};
