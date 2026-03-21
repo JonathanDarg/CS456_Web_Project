e687982e43a8ed917042c63df45678943de5835b
jonathanDargakis <jmdargakis@loyola.edu>
2026-03-20 20:42:46 -0400

    Save Dogs Dao and Controller basic

---
+++ model/SaveDogDao.js
@@ -0,0 +1,35 @@
+let lstDogs = [
+  {_id: 1, name: 'Buddy', breed: 'Golden Retriever', age: '3', description: 'Friendly and great with families'},
+  {_id: 2, name: 'Max', breed: 'German Shepherd', age: '5', description: 'Loyal and highly intelligent working dog'},
+  {_id: 3, name: 'Bella', breed: 'Bulldog', age: '4', description: 'Calm, courageous, and loves lounging'},
+  {_id: 4, name: 'Charlie', breed: 'Beagle', age: '2', description: 'Curious and energetic with a great sense of smell'},
+  {_id: 5, name: 'Lucy', breed: 'Poodle', age: '6', description: 'Very smart and hypoallergenic coat'},
+  {_id: 6, name: 'Rocky', breed: 'Rottweiler', age: '7', description: 'Confident and protective companion'},
+  {_id: 7, name: 'Daisy', breed: 'Dachshund', age: '1', description: 'Playful and brave with a long body'},
+  {_id: 8, name: 'Luna', breed: 'Siberian Husky', age: '3', description: 'Energetic with striking blue eyes'}
+];
+
+
+// Create and save a new dog 
+exports.createDog = async (dog) => {
+
+};
+
+// Get all Dog
+exports.getAllDogs = async () => {
+};
+
+// Get an Dog by its ID
+exports.getDogById = async (id) => {
+
+};
+
+// Update Sogs info
+exports.update = function(dog){
+
+}
+
+// Delete an Dog by its ID
+exports.deleteDog = async (id) => {
+
+};
