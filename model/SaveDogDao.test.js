064fdb7c4cb21c9387e009b5a451fc68a90eeade
Meghan Burton <meburton@loyola.edu>
2026-04-03 20:53:54 -0400

    update delete test

--- model/SaveDogDao.test.js
+++ model/SaveDogDao.test.js
@@ -1,45 +1,53 @@
-const dao = require('./SaveDogDao');
+const dao = require("./SaveDogDao");
 
-test("Always green test", function(){
-    expect(1).toBe(1);
+test("Always green test", function () {
+  expect(1).toBe(1);
 });
 
-test("ReadAll Mem has the predefined dogs", function(){
-    let lstDogs = dao.getAllDogs();
+test("ReadAll Mem has the predefined dogs", function () {
+  let lstDogs = dao.getAllDogs();
 
-    expect(lstDogs.length).toBe(8);
+  expect(lstDogs.length).toBe(8);
 });
 
-test("Create a new dog", function(){
-    let newdog = {name:"Pudgy", breed:"Chihuahua", age:9, gender:"Male", description:"Full of energy"};
+test("Create a new dog", function () {
+  let newdog = {
+    name: "Pudgy",
+    breed: "Chihuahua",
+    age: 9,
+    gender: "Male",
+    description: "Full of energy",
+  };
 
-    let created = dao.createDog(newdog); 
-    let found = dao.getDogById(created._id);
+  let created = dao.createDog(newdog);
+  let found = dao.getDogById(created._id);
 
-    expect(created._id).toBe(found._id); 
-    expect(created.name).toBe(found.name);
+  expect(created._id).toBe(found._id);
+  expect(created.name).toBe(found.name);
 
-    dao.deleteDog(created.id); 
-
+  dao.deleteDog(created.id);
 });
 
-test("Update dog", function() {
-    let newdog = { // create the dog 
-        name: "Luke",
-        breed: "Golden Retreiver",
-        age: 4,
-        gender: "Male",
-        description: "Energetic"
-    };
+test("Update dog", function () {
+  let newdog = {
+    // create the dog
+    name: "Luke",
+    breed: "Golden Retreiver",
+    age: 4,
+    gender: "Male",
+    description: "Energetic",
+  };
 
-    let create = dao.createDog(newdog); // create the new dog
-    let found = dao.getDogById(created._id); // make sure that it exists
-    expect(found.name).toBe("Thor"); // assert name
+  let create = dao.createDog(newdog); // create the new dog
+  let found = dao.getDogById(created._id); // make sure that it exists
+  expect(found.name).toBe("Thor"); // assert name
 
-    dao.update(found); // update dog
-    
+  dao.update(found); // update dog
 });
 
-test("Delete dog", function()) {
+test("Delete dog", function () {
+  let created = dao.createDog(newdog); // create the dog
 
-};
+  let found = dao.getDogById(created._id);
+  expect(found);
+});
