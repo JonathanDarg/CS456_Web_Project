c90d281ff582a0b0b2449d503950a3a38b16b818
jonathanDargakis <jmdargakis@loyola.edu>
2026-04-03 23:31:20 -0400

    fixed tests and added invalid id update test

--- model/SaveDogDao.test.js
+++ model/SaveDogDao.test.js
@@ -25,45 +25,49 @@ test("Create a new dog", function () {
   expect(created._id).toBe(found._id);
   expect(created.name).toBe(found.name);
 
-  dao.deleteDog(created.id);
+  dao.deleteDog(created._id); // cleans up list of dogs
 });
 
 test("Update dog", function () {
-  let dog = {
-    // create the dog
-    name: "Luke",
-    breed: "Golden Retreiver",
-    age: 4,
-    gender: "Male",
-    description: "Energetic",
-  };
-
-  let newdog = dao.createDog(dog); // create the new dog
-
-  let found = dao.getDogById(newdog._id);
-  expect(newdog.name).toBe(found.name); // make sure that it exists
+  let dog = dao.getDogById(1);
+  expect(dog.name).toBe("Buddy"); 
 
   //change the properties of the dog
-  newdog.name = "Thor";
-  newdog.age = 5;
+  dog.name = "Thor";
+  dog.age = 5;
 
-  dao.update(newdog); // update dog in DAO
+  dao.update(dog); // update dog in DAO
 
-  let updated = dao.getDogById(newdog._id); // get the updated dog
+  let updated = dao.getDogById(dog._id); // get the updated dog
 
   //check that the changes were made and saved
   expect(updated.name).toBe("Thor");
   expect(updated.age).toBe(5);
 
-  // now delete the original dog
-  dao.deleteDog(newdog._id);
+});
+
+test("Update dog by invalid id", function () {
+  let fakeDog = {
+    _id: 10, 
+    name: "Pudgyyy",
+    breed: "Chihuahua",
+    age: 9,
+    gender: "Male",
+    description: "Full of energy",
+  };
+
+  //try to update a dog with an id that does not exist
+  dao.update(fakeDog);
+
+  // dog was not updated or added to list of dogs, so it does nothing
+  expect(dao.getAllDogs().length).toBe(8);
 });
 
 test("Delete dog", function () {
   let lstDogs = dao.getAllDogs(); // pick an existing dog from the website
 
   let dogDelete = lstDogs[0]; // get the first dog
-  expect(dogDelete.name).toBe("Buddy");
+  expect(dogDelete.name).toBe("Thor");
 
   dao.deleteDog(dogDelete._id); // delete the dog
   // expect the dog name to be null
