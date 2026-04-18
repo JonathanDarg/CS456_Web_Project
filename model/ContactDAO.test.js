c7995149457b814382179ed368eaa766cdc67838
Meghan Burton <meburton@loyola.edu>
2026-04-17 22:34:34 -0400

    ReadAll contacts calls dao.readAll and makes sure it returns a list.

--- model/ContactDAO.test.js
+++ model/ContactDAO.test.js
@@ -3,3 +3,8 @@ const dao = require("./ContactDAO");
 test("Always green test", function () {
   expect(1).toBe(1);
 });
+
+test("ReadAll contacts", async function () {
+  let lstcontacts = await dao.readAll();
+  expect(lstcontacts.length).toBe(lstcontacts.length);
+});
