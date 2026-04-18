86614bf7207fc9f8c7f81e31ede3a356d046c24c
Meghan Burton <meburton@loyola.edu>
2026-04-17 22:38:16 -0400

    create a new contact, save the contact, and retrieve it by ID

--- model/ContactDAO.test.js
+++ model/ContactDAO.test.js
@@ -8,3 +8,13 @@ test("ReadAll contacts", async function () {
   let lstcontacts = await dao.readAll();
   expect(lstcontacts.length).toBe(lstcontacts.length);
 });
+
+test("Create new contact", async function () {
+  let newcontact = {
+    name: "Buddy",
+    description: "Hyper",
+  };
+
+  let created = await dao.create(newcontact);
+  let found = await dao.read(created._id);
+});
